33aa20e1fba64b4e8f71e74032ac8059673207b2
Alexander Zolotarev <megabogus84@gmail.com>
2021-02-26 12:20:12 +0300

    Update defaults.py

--- django_history/defaults.py
+++ django_history/defaults.py
@@ -1,6 +1,6 @@
 import itertools
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from .utils import observe
 
 
